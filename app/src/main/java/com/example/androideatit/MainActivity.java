3c068ad458c42505899b1ba043e88fc0f17dfdf3
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-10-05 00:19:44 -0600

    Aplicación de gestión de órdenes/comandas para restaurante - EatIt

---
+++ app/src/main/java/com/example/androideatit/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.androideatit;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSignIn, btnSignUp;
+
+    TextView txtSlogan;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ******************* Instancias a los componentes graficos *********************
+
+        btnSignIn = (Button) findViewById (R. id.btnSignIn);
+        btnSignIn.setOnClickListener(this);
+        btnSignUp = (Button) findViewById (R. id.btnSignUp);
+        btnSignUp.setOnClickListener(this);
+
+        txtSlogan = (TextView) findViewById (R. id. txtSlogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnSignIn: {
+
+            }break;
+            case R.id.btnSignUp: {
+            }break;
+        }
+    }
+}
